04479781e4ac7a7f1d2b4c5e53ed174f3c6176a4
thehieu236 <thehieu236@gmail.com>
2023-08-12 13:33:41 +0700

    Fix filter

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,125 +4,122 @@ import MonAn from "./../models/MonAn.js";
 import Menu from "./../models/Menu.js";
 
 // tao 1 doi tuong menu tu lop doi tuong Menu
-let menu = new Menu()
-menu.layTuLocalStorage()
-console.log(menu);
+let menu = new Menu();
+menu.layTuLocalStorage();
+// console.log(menu);
 
-document.getElementById('foodForm').onsubmit = () => {
-    // dung event.preventDefault()
-    event.preventDefault();
-    console.log('hello');
-    // goi ra 1 doi tuong tu lop doi tuong 
-    let monAn = new MonAn();
-    console.log(monAn);
-    // lay du lieu tu ng dung
-    // vong lap de  di qua tung phan tu co trong mang
-    //for .. in
-    // lay tat ca cac input trong form co id la foodform
-    let arrField = document.querySelectorAll('#foodForm input , #foodForm select , #foodForm textarea');// ==> tra ve mang
-    console.log(arrField);
-    // dung vong lap for of // dom toi nhung input, select va textarea
-    for (let field of arrField) {
-        console.log(field);
-        // let value = field.value;
-        // let id = field.id
-        // dung destructuring
-        let { value, id } = field;// == line 22 va 23
-        //id la ten thuoc tinh dong, no co the di chuyen
-        monAn[id] = value;
-    }
-    // .value .id
-    console.log(monAn);
-    // goi ra phuong thuc de them monAn vao mang
-    menu.themMonAn(monAn);
-    // for in : duyet qua tung ptu trong mang, nhung gia tri tra ve vi tri index
-    // let arrNumber = [1,2,3,4 ]
-    // for(let index in arrNumber){
-    //     console.log(index);
-    // }
-    // for (let item of arrNumber){
-    //     console.log(item); // phan tu trong mang
-    // }
-    // // for of tra ve tung phan tu co trong mang
-
-}
-
+document.getElementById("foodForm").onsubmit = () => {
+  // dung event.preventDefault()
+  event.preventDefault();
+  //   console.log("hello");
+  // goi ra 1 doi tuong tu lop doi tuong
+  let monAn = new MonAn();
+  //   console.log(monAn);
+  // lay du lieu tu ng dung
+  // vong lap de  di qua tung phan tu co trong mang
+  //for .. in
+  // lay tat ca cac input trong form co id la foodform
+  let arrField = document.querySelectorAll(
+    "#foodForm input , #foodForm select , #foodForm textarea"
+  ); // ==> tra ve mang
+  //   console.log(arrField);
+  // dung vong lap for of // dom toi nhung input, select va textarea
+  for (let field of arrField) {
+    // console.log(field);
+    // let value = field.value;
+    // let id = field.id
+    // dung destructuring
+    let { value, id } = field; // == line 22 va 23
+    //id la ten thuoc tinh dong, no co the di chuyen
+    monAn[id] = value;
+  }
+  // .value .id
+  //   console.log(monAn);
+  // goi ra phuong thuc de them monAn vao mang
+  menu.themMonAn(monAn);
+  // for in : duyet qua tung ptu trong mang, nhung gia tri tra ve vi tri index
+  // let arrNumber = [1,2,3,4 ]
+  // for(let index in arrNumber){
+  //     console.log(index);
+  // }
+  // for (let item of arrNumber){
+  //     console.log(item); // phan tu trong mang
+  // }
+  // // for of tra ve tung phan tu co trong mang
+};
 
 // cách  đàu tiên khai báo cho 1 đối tượng window sự kiện xoá món ăn
 window.xoaMonAn = function (id) {
-    console.log(id);
-    menu.xoaMonAn(id)
+  //   console.log(id);
+  menu.xoaMonAn(id);
 };
-console.log(window);
+// console.log(window);
 
 window.suaMonAn = function (id) {
-    console.log(id);
-    // gọi tới đối tượng menu và lấy ra món ăn tìm kiếm
-    let monAn = menu.layThongTinMonAn(id);
-    // món ăn cần chỉnh sửa
-    if (monAn) {
-        // mở modal lên
-        console.log(monAn);
-        document.getElementById('btnThem').click();
-        let arrField = document.querySelectorAll(
-            '#foodForm input,#foodForm select, #foodForm textarea'
-        );
-        console.log(arrField);
-        console.log(monAn);
-        // for...of
-        for (let item of arrField) {
-            // input id là foodId
-            // item là từng cái DOM tới các input, select và textarea
-            item.value = monAn[item.id]; // item đại diện cho từng input... trong form, nên có thể bóc id từ item để xác định lấy dữ liệu từ thuộc tính nào trong đối tượng món ăn
-        }
-
-        // ngăn chặn người dùng không được chỉnh sửa mã món
-        document.getElementById('foodID').readOnly = true;
+  //   console.log(id);
+  // gọi tới đối tượng menu và lấy ra món ăn tìm kiếm
+  let monAn = menu.layThongTinMonAn(id);
+  // món ăn cần chỉnh sửa
+  if (monAn) {
+    // mở modal lên
+    // console.log(monAn);
+    document.getElementById("btnThem").click();
+    let arrField = document.querySelectorAll(
+      "#foodForm input,#foodForm select, #foodForm textarea"
+    );
+    // console.log(arrField);
+    // console.log(monAn);
+    // for...of
+    for (let item of arrField) {
+      // input id là foodId
+      // item là từng cái DOM tới các input, select và textarea
+      item.value = monAn[item.id]; // item đại diện cho từng input... trong form, nên có thể bóc id từ item để xác định lấy dữ liệu từ thuộc tính nào trong đối tượng món ăn
     }
+
+    // ngăn chặn người dùng không được chỉnh sửa mã món
+    document.getElementById("foodID").readOnly = true;
+  }
 };
 
 // cap nhat thog tin mon an
 document.getElementById("btnCapNhat").onclick = () => {
-    console.log('object');
-    // lay du lieu tu ng dung ve 
-    // tao ra 1 doi tuong mon an de luu tru du lieu ng dung da chinh sua
-    let monAn = new MonAn();
-    let arrField = document.querySelectorAll('#foodForm input,#foodForm select, #foodForm textarea');
-    console.log(arrField);
-    // for ... of:
-    for (let item of arrField) {
-        // goi lay ra id va value tu item, dai dien cho tung dom ben trong arrField
-        let { id, value } = item;
-        monAn[id] = value
-    }
-    console.log(monAn);
-    menu.capNhatMonAn(monAn)
-    document.querySelector('.modal-footer .btn-secondary').click()
-    document.getElementById("foodForm").reset()
-    document.getElementById("foodForm").readOnly = false
-    // khi cap nhat xong nho tat modal, clear form de lan sau ko con du lieu do,
-    // mo readOnly cho input foodID
+  //   console.log("object");
+  // lay du lieu tu ng dung ve
+  // tao ra 1 doi tuong mon an de luu tru du lieu ng dung da chinh sua
+  let monAn = new MonAn();
+  let arrField = document.querySelectorAll(
+    "#foodForm input,#foodForm select, #foodForm textarea"
+  );
+  //   console.log(arrField);
+  // for ... of:
+  for (let item of arrField) {
+    // goi lay ra id va value tu item, dai dien cho tung dom ben trong arrField
+    let { id, value } = item;
+    monAn[id] = value;
+  }
+  //   console.log(monAn);
+  menu.capNhatMonAn(monAn);
+  document.querySelector(".modal-footer .btn-secondary").click();
+  document.getElementById("foodForm").reset();
+  document.getElementById("foodForm").readOnly = false;
+  // khi cap nhat xong nho tat modal, clear form de lan sau ko con du lieu do,
+  // mo readOnly cho input foodID
 };
 // lọc món ăn dụa theo loại món
 
-let selectElement = document.getElementById("loai");
+let selectElement = document.getElementById("selLoai");
 
 selectElement.onchange = (event) => {
-    console.log('ff');
-
-    // dung event.target de dom toiw the co su kein onchange
-    let { value } = event.target;
-    let arrFilter = [];
-    if (value == 'all') {
-        arrFilter = menu.arrMonAn;
-        console.log(arrFilter);
-    } else {
-        arrFilter = menu.arrMonAn.filter((item) => item.loai == value);
-        console.log(arrFilter);
-    };
-    // neu nhu value = "all" thi se ko filter loc va lay tat ca
-    // dung value de loc ra cac mon an theo ng dung muon, ham fileter se tra ve mang ms
-    menu.renderMonAn(arrFilter);
-}
-
-
+  // dung event.target de dom toiw the co su kein onchange
+  let { value } = event.target;
+  let arrFilter = [];
+  if (value == "all") {
+    arrFilter = menu.arrMonAn;
+  } else {
+    arrFilter = menu.arrMonAn.filter((item) => item.loai == value);
+    console.log("object");
+  }
+  // neu nhu value = "all" thi se ko filter loc va lay tat ca
+  // dung value de loc ra cac mon an theo ng dung muon, ham fileter se tra ve mang ms
+  menu.renderMonAn(arrFilter);
+};
--- app/models/Menu.js
+++ app/models/Menu.js
@@ -1,128 +1,127 @@
 import MonAn from "./MonAn.js";
 // import MonAn  from "./MonAn.js";
 class Menu {
-    // se la noi chua mang tat ca cac mon an va cac phuong thuc xu ly vs mang do
-    arrMonAn = [];
+  // se la noi chua mang tat ca cac mon an va cac phuong thuc xu ly vs mang do
+  arrMonAn = [];
 
-    // themMonAn. render mon an len giao dien, xoa mon an, cap nhat mon an, luu xuong local va goi du lieu tu local len, tim kiem va loc theo yeu cau
+  // themMonAn. render mon an len giao dien, xoa mon an, cap nhat mon an, luu xuong local va goi du lieu tu local len, tim kiem va loc theo yeu cau
 
-    //THEM MON AN
-    themMonAn = (monAn) => {
-        this.arrMonAn.push(monAn);
-        this.renderMonAn();
-        this.luuXuongLocalStorage()
-    };
-    //  render du lieu, default parameter
-    renderMonAn = (arr = this.arrMonAn) => {// lay dc object
-        // dung destrucring
-        let content = "";
-        for (let i = 0; i < arr.length; i++) {
-            let monAn = new MonAn()
-            console.log(arr[i]);
-            // monAn ={...monAn, ... arr[i]}
-            Object.assign(monAn, arr[i])
+  //THEM MON AN
+  themMonAn = (monAn) => {
+    this.arrMonAn.push(monAn);
+    this.renderMonAn();
+    this.luuXuongLocalStorage();
+  };
+  //  render du lieu, default parameter
+  renderMonAn = (arr = this.arrMonAn) => {
+    // lay dc object
+    // dung destrucring
+    let content = "";
+    for (let i = 0; i < arr.length; i++) {
+      let monAn = new MonAn();
+      // console.log(arr[i]);
+      // monAn ={...monAn, ... arr[i]}
+      Object.assign(monAn, arr[i]);
 
-            // console.log(monAn);
-            let { foodID, tenMon, loai, giaMon, khuyenMai, tinhTrang, tinhGiaKhuyenMai, } = monAn;
-            content += `
+      // console.log(monAn);
+      let {
+        foodID,
+        tenMon,
+        loai,
+        giaMon,
+        khuyenMai,
+        tinhTrang,
+        tinhGiaKhuyenMai,
+      } = monAn;
+      content += `
             
 <tr>
 <td>${foodID}</td>
 <td>${tenMon}</td>
-<td>${loai == 'loai1' ? 'chay' : 'man'}</td>
+<td>${loai == "loai1" ? "Chay" : "Mặn"}</td>
 <td>${giaMon}</td>
-<td>${khuyenMai}</td>
+<td>${khuyenMai}%</td>
 <td>${tinhGiaKhuyenMai()}</td>
-<td>${tinhTrang == "0" ? "het" : "con"}</td>
+<td>${tinhTrang == "0" ? "Hết" : "Còn"}</td>
 <td>
 <button class="btn btn-danger"  onclick='xoaMonAn("${foodID}")'>Xoa</button>
 <button class="btn btn-warning"  onclick='suaMonAn("${foodID}")'>Sua</button>
 </td>
 </tr>
 
-`
-        }
-        document.getElementById("tbodyFood").innerHTML = content
-    };
-    // class Menu {
-    // luu xuong local
-    luuXuongLocalStorage = () => {
-        // chuyen mang thanh chuoi Json
-        let chuoiJson = JSON.stringify(this.arrMonAn)
-        localStorage.setItem('arrMonAn', chuoiJson);
-    };
-    // lay tu local len
-    layTuLocalStorage = () => {
-        // goi du lieu tu local len
-        let arrMonAn = localStorage.getItem('arrMonAn');
-        // parse chuoi json ve lai kieu du lieu ban dau
-        //parse xong se gan gia ri vao ben trong arrMonAn cua lop doi tuong
-        if (arrMonAn) {
-            this.arrMonAn = JSON.parse(arrMonAn);
-            this.renderMonAn();
-        }
-
-    };
-    // phuong thuc xoa mang khoi mon an
-    xoaMonAn = (id) => {
-        // find index su dung vs arrow function
-        console.log(this.arrMonAn)
-
-        // find index==> timf ra dc vi tri index phan tu chung ta muon tim, // index la vi tri index trong mang// item la phan tu
-        let index = this.arrMonAn.findIndex((item) => item.foodID == id)
-        //VI DU VE FINDINEX
-        // const array1 = [5, 12, 8, 130, 44];
-
-        // const isLargeNumber = (element) => element > 13;
-
-        // console.log(array1.findIndex(isLargeNumber));
-        // Expected output: 3
-        //  co 1 bieu thuc so sanh de tim ra dc item muon tim
-        // th1: tim dc phan tu ma chung ta muon
-        //th2: tim ko co phan tu do trong mang ==> index= -1
-        if (index !== -1) {
-            this.arrMonAn.splice(index, 1)
-            this.renderMonAn(); // TRUONG HOP 1
-            this.luuXuongLocalStorage();
-        }
-        //    this.arrMonAn.splice(id, 1)
-    };
-
-    // tim thong tin ng dung muon sua , sau do hien thi len form
-    layThongTinMonAn = (id) => {
-        //chay vong lap arrMonAn
-        // dung id de xac dinh phan tu can lay
-        let monAn = this.arrMonAn.find(function (item, index) {
-            // ham find se giup lay ra phan tu thoa dk trong mang
-            return item.foodID == id;
-        });
-        // th1: tim dc phhan tu chung ta muon, th2: ko co phan tu trong mang: undefined
-        // sua khi ng dung chinh sua xong, lay het thong tin va update lai mon an do
-        if (monAn) {
-            return monAn;
-        }
+`;
     }
-
-    capNhatMonAn(monAn) {
-        // tim vi tri index cua mon can sua, su dung findIndex
-        let index = this.arrMonAn.findIndex((item) => item.foodID == monAn.foodID)
-        if (index !== -1) {
-            this.arrMonAn[index] = monAn;
-            this.luuXuongLocalStorage()
-            this.renderMonAn();
-        }
+    document.getElementById("tbodyFood").innerHTML = content;
+  };
+  // class Menu {
+  // luu xuong local
+  luuXuongLocalStorage = () => {
+    // chuyen mang thanh chuoi Json
+    let chuoiJson = JSON.stringify(this.arrMonAn);
+    localStorage.setItem("arrMonAn", chuoiJson);
+  };
+  // lay tu local len
+  layTuLocalStorage = () => {
+    // goi du lieu tu local len
+    let arrMonAn = localStorage.getItem("arrMonAn");
+    // parse chuoi json ve lai kieu du lieu ban dau
+    //parse xong se gan gia ri vao ben trong arrMonAn cua lop doi tuong
+    if (arrMonAn) {
+      this.arrMonAn = JSON.parse(arrMonAn);
+      this.renderMonAn();
     }
+  };
+  // phuong thuc xoa mang khoi mon an
+  xoaMonAn = (id) => {
+    // find index su dung vs arrow function
+    console.log(this.arrMonAn);
 
+    // find index==> timf ra dc vi tri index phan tu chung ta muon tim, // index la vi tri index trong mang// item la phan tu
+    let index = this.arrMonAn.findIndex((item) => item.foodID == id);
+    //VI DU VE FINDINEX
+    // const array1 = [5, 12, 8, 130, 44];
 
+    // const isLargeNumber = (element) => element > 13;
+
+    // console.log(array1.findIndex(isLargeNumber));
+    // Expected output: 3
+    //  co 1 bieu thuc so sanh de tim ra dc item muon tim
+    // th1: tim dc phan tu ma chung ta muon
+    //th2: tim ko co phan tu do trong mang ==> index= -1
+    if (index !== -1) {
+      this.arrMonAn.splice(index, 1);
+      this.renderMonAn(); // TRUONG HOP 1
+      this.luuXuongLocalStorage();
+    }
+    //    this.arrMonAn.splice(id, 1)
+  };
+
+  // tim thong tin ng dung muon sua , sau do hien thi len form
+  layThongTinMonAn = (id) => {
+    //chay vong lap arrMonAn
+    // dung id de xac dinh phan tu can lay
+    let monAn = this.arrMonAn.find(function (item, index) {
+      // ham find se giup lay ra phan tu thoa dk trong mang
+      return item.foodID == id;
+    });
+    // th1: tim dc phhan tu chung ta muon, th2: ko co phan tu trong mang: undefined
+    // sua khi ng dung chinh sua xong, lay het thong tin va update lai mon an do
+    if (monAn) {
+      return monAn;
+    }
+  };
+
+  capNhatMonAn(monAn) {
+    // tim vi tri index cua mon can sua, su dung findIndex
+    let index = this.arrMonAn.findIndex((item) => item.foodID == monAn.foodID);
+    if (index !== -1) {
+      this.arrMonAn[index] = monAn;
+      this.luuXuongLocalStorage();
+      this.renderMonAn();
+    }
+  }
 }
 
-
-
-
-
-
 export default Menu;
-
-
 
 /// MAN
